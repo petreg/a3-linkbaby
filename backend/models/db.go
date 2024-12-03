ebdfbc3079e21321ed4eac80231c452a38537131
Luiz Régis <luiz.regis@intelbras.com.br>
2024-12-03 00:12:12 -0300

    alterado o banco para sqlite

--- backend/models/db.go
+++ backend/models/db.go
@@ -2,43 +2,16 @@ package models
 
 import (
 	"log"
-"fmt"
-	"gorm.io/driver/mysql"
+
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"os"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	
-	dbHost := os.Getenv("DB_HOST")
-	if dbHost == "" {
-		dbHost = "localhost"
-	}
+	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
 
-	dbPort := os.Getenv("DB_PORT")
-	if dbPort == "" {
-		dbPort = "3306"
-	}
-
-	dbUser := os.Getenv("DB_USER")
-	if dbUser == "" {
-		dbUser = "linkbaby" 
-	}
-
-	dbPass := os.Getenv("DB_PASSWORD")
-	if dbPass == "" {
-		dbPass = "linkbaby" 
-	}
-
-
-
-	dsn := dbUser + ":" + dbPass + "@tcp(" + dbHost + ":" + dbPort + ")/linkbaby?charset=utf8mb4&parseTime=True&loc=Local"
-
-	fmt.Println(dsn)
-
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
@@ -62,3 +35,5 @@ func ConnectDatabase() {
 
 	DB = db
 }
+
+
